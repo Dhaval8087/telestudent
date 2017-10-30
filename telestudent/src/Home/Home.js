c3fa5c196485ef3ba459a57fa54fbd23fe8fefa3
ZEROCHAOS\dpatel2 <dpatel2@zerochaos.com>
2017-10-30 16:03:06 +0530

    formateed code

--- telestudent/src/Home/Home.js
+++ telestudent/src/Home/Home.js
@@ -1,8 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Datastore from 'nedb';
 import MetaData from '../Data';
-import {Tex} from 'react-tex';
+import { Tex } from 'react-tex';
+import './Home.css';
 class Home extends Component {
     constructor(props) {
         super(props);
@@ -21,23 +22,17 @@ class Home extends Component {
 
     }
     loadData() {
-        var blocks = new Datastore({filename: 'Blocks.db', autoload: true});
+        var blocks = new Datastore({ filename: 'Blocks.db', autoload: true });
 
-        blocks
-            .find({})
-            .exec(function (err, block) {
-                block
-                    .forEach(function (d) {
-                        this
-                            .state
-                            .data
-                            .push(d);
-                    }.bind(this));
-                this.setState({});
+        blocks.find({}).exec(function (err, block) {
+            block.forEach(function (d) {
+                this.state.data.push(d);
             }.bind(this));
+            this.setState({});
+        }.bind(this));
     }
     insertData() {
-        var blocks = new Datastore({filename: 'Blocks.db', autoload: true});
+        var blocks = new Datastore({ filename: 'Blocks.db', autoload: true });
         blocks.remove({}, {
             multi: true
         }, function (err, num) {
@@ -54,11 +49,11 @@ class Home extends Component {
         block = {
             metadataId: 2,
             value: 'The <P> element is used to define a paragraph. The exact rendering (indentation,' +
-                    ' leading etc.) is not defined and may be a function of other tags, style sheets,' +
-                    ' etc. The ALIGN attribute can be used to explicitly specify the horizontal align' +
-                    'ment. Paragraph elements have the same content model as headers, that is text an' +
-                    'd character level markup, such as character emphasis, inline images, form fields' +
-                    ' and math. '
+            ' leading etc.) is not defined and may be a function of other tags, style sheets,' +
+            ' etc. The ALIGN attribute can be used to explicitly specify the horizontal align' +
+            'ment. Paragraph elements have the same content model as headers, that is text an' +
+            'd character level markup, such as character emphasis, inline images, form fields' +
+            ' and math. '
         };
         blocksData.push(block);
 
@@ -72,20 +67,16 @@ class Home extends Component {
         }.bind(this));
 
     }
-    findTagName(tagId)
-    {
+    findTagName(tagId) {
         let htmlTag = '';
-        MetaData
-            .metaData
-            .map(item => {
-                if (item.id === tagId) 
-                    htmlTag = item.htmlTag;
-                }
-            )
+        MetaData.metaData.map(item => {
+            if (item.id === tagId)
+                htmlTag = item.htmlTag;
+        }
+        )
         return htmlTag;
     }
     render() {
-        let latex = "\int_{a}^{b} f(x)dx = F(b) - F(a)";
         return (
             <div className="container">
                 <div className="row">
@@ -95,23 +86,35 @@ class Home extends Component {
                 </div>
                 <div className="row">
                     <div className="col-md-12 text-center">
-                        {this
-                            .state
-                            .data
-                            .map(item => {
-                                var Tag
-                                Tag = this.findTagName(item.metadataId);
-                                if (item.metadataId === 3) {
-                                    return <Tex texContent={item.value}/>
-                                } else {
-                                    return <Tag>{item.value}</Tag>
-                                }
+                        {this.state.data.map(item => {
+                            var Tag
+                            Tag = this.findTagName(item.metadataId);
+                            if (item.metadataId === 3) {
+                                return <Tex texContent={item.value} />
+                            } else {
+                                return <Tag>{item.value}</Tag>
+                            }
 
-                            })
-}
+                        })
+                        }
                     </div>
-
                 </div>
+                <div className="row">
+                <FlipPage>
+  <article>
+    <h1>My awesome first article</h1>
+    <p>My awesome first content</p>
+  </article>
+  <article>
+    <h1>My wonderful second article</h1>
+    <p>My wonderful second content</p>
+  </article>
+  <article>
+    <h1>My excellent third article</h1>
+    <p>My excellent third content</p>
+  </article>
+</FlipPage>
+                    </div>
             </div>
         );
     }
